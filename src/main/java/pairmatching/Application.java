b4d02f9e8ff3b374467baa405e9592bc01b717ff
mainfn <_@mainfn.dev>
2023-09-18 11:27:01 +0900

    feat: Application 작성

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,26 @@
 package pairmatching;
 
-public class Application {
-    public static void main(final String[] args) {
-        // TODO 구현 진행
+import pairmatching.controller.MainController;
+import pairmatching.domain.CrewRepository;
+import pairmatching.domain.PairMatcher;
+import pairmatching.domain.PairRepository;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+public final class Application {
+
+  public static void main(final String[] args) {
+    final InputView inputView = new InputView();
+    final OutputView outputView = new OutputView();
+    final PairRepository pairRepository = new PairRepository();
+    final CrewRepository crewRepository = CrewRepository.create();
+    final PairMatcher pairMatcher = new PairMatcher(pairRepository, crewRepository);
+    final MainController mainController = new MainController(outputView, inputView, pairMatcher);
+
+    try {
+      mainController.start();
+    } catch (final Exception e) {
+      System.out.printf("[ERROR] %s\n", e.getMessage());
     }
+  }
 }
